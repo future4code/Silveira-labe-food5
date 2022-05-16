1617548a1b60419f77793a4719b68725207706b5
berbraga <bernardo.sbraga@yahoo.com>
2022-05-16 13:13:15 -0300

    iniciando

---
+++ src/Context/GlobalState.js
@@ -0,0 +1,22 @@
+import React,  { useState } from 'react'
+import axios from 'axios'
+import { GlobalStateContext } from './GlobalStateContext'
+
+
+
+export default function GlobalState(props) {
+	
+    
+    
+    
+    const states = {   }
+	const setters = {}
+	const requests = {}
+	const func = {}
+
+    return (
+		<GlobalStateContext.Provider value={{states,setters,requests,func}}>
+			{props.children}
+		</GlobalStateContext.Provider>
+	)
+}
