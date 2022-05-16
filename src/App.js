b1068f5291d05b3c6d68d30a19c62a972966e2f9
Andresa Alves <andresa_15ga@hotmail.com>
2022-05-16 19:01:57 -0300

    tela Inicial quase concluída

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, NomeContainer, Nome1, Nome2 } from "./style";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <NomeContainer>
+        <Nome1>Future</Nome1>
+
+        <Nome2>Eats</Nome2>
+      </NomeContainer>
+    </Container>
   );
 }
-
-export default App;
