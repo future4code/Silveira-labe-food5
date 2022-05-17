617f1c7a585c483f515518baedf747ef384e8d4a
Andresa Alves <andresa_15ga@hotmail.com>
2022-05-17 15:30:29 -0300

    Tela Inicial e Login quase 100% concluídos

---
+++ src/Pages/Login/TelaInicial.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, NomeContainer, Nome1, Nome2 } from "./TelaInicialStyle";
+
+export default function HomePage() {
+  return (
+    <Container>
+      <NomeContainer>
+        <Nome1>Future</Nome1>
+
+        <Nome2>Eats</Nome2>
+      </NomeContainer>
+    </Container>
+  );
+}
