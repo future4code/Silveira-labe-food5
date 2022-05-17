a855cf3e4888535cca60f87b2158f90dafe0a7eb
berbraga <bernardo.sbraga@yahoo.com>
2022-05-17 10:54:06 -0300

    Bernardo: resolvido o Router

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,16 +1,18 @@
 
+import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from '../Pages/Home/HomePage';;
-// import LoginPage from '../Pages/Login/LoginPage';
+import HomePage from '../Pages/Home/HomePage';
+import LoginPage from '../Pages/Login/LoginPage';
 
 export const Router = () => {
-
-
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<HomePage />} />
+                <Route path="/login" element={ <LoginPage  /> } />
             </Routes>
         </BrowserRouter>
     );
 }
+export default Router
